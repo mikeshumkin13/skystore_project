ed372a99a901abb859fefac6406c0c32d75e8d44
a12345 <shu.m2010@yandex.ru>
2025-05-11 00:16:12 +0300

    Удалён views.py, обновлены импорты в urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from . import views
+from .views.home import home, add_product
+from .views.product import product_detail
+from .views.contacts import contacts, thank_you
 
 app_name = "catalog"
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("contacts/", views.contacts, name="contacts"),
-    path("product/<int:pk>/", views.product_detail, name="product_detail"),
-    path("thank-you/", views.thank_you, name="thank_you"),
-]
+    path("", home, name="home"),
+    path("add-product/", add_product, name="add_product"),
+    path("contacts/", contacts, name="contacts"),
+    path("thank-you/", thank_you, name="thank_you"),
+    path("product/<int:pk>/", product_detail, name="product_detail"),
 
