ea777dd7c00613488d0c4ada3c7e18e589fd75ca
a12345 <shu.m2010@yandex.ru>
2025-06-03 15:03:33 +0300

    install redis add settings.py redis

--- skystore/settings.py
+++ skystore/settings.py
@@ -159,3 +159,18 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 LOGIN_REDIRECT_URL = "/users/profile/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/users/login/"
+
+
+
+CACHE_ENABLED = True
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
